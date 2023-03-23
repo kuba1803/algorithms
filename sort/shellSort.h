6e93b0b7cf49d01f86193225de3b2cf02fe395fc
Jakub <kuba1803@ymail.com>
2023-03-23 22:37:14 +0100

    improve shellSort

--- sort/shellSort.h
+++ sort/shellSort.h
@@ -14,15 +14,21 @@ namespace SORT {
 
     template<typename Container, typename Comparator>
     inline void shellSort(Container &container, int start, int end, Comparator comper) {
-        int h = 1;
-        while (h < (end - start) / 3)h = 3 * h + 1;
-        while (h >= 1) {
-            for (int i = h; i < (end - start); i++) {
-                for (int j = i; j >= h && comper(container[j], container[j - h]); j -= h) {
-                    std::swap(container[j], container[j - h]);
+        int gap, i = 1;
+        std::stack<int> gaps;
+        do {
+            gap = std::ceil(1.8 * std::pow(2.25, i - 1) - 0.8);
+            i++;
+        } while (gap < (end - start));
+        i--;
+        while (i > 0) {
+            gap = std::ceil(1.8 * std::pow(2.25, i - 1) - 0.8);
+            for (int i = gap; i < (end - start); i++) {
+                for (int j = i; j >= gap && comper(container[j], container[j - gap]); j -= gap) {
+                    std::swap(container[j], container[j - gap]);
                 }
             }
-            h /= 3;
+            i--;
         }
     }
 }
